9ceb0b8e2d4860edbc02e3e469e6523a9d615d1d
Arbi <arbihamolli111@gmail.com>
2025-08-27 23:30:42 +0200

    Update App.jsx
    Added TURN server for the calling purpose

--- src/App.jsx
+++ src/App.jsx
@@ -340,7 +340,17 @@ const App = () => {
         }
         mediaStreamRef.current = stream;
 
-        const pc = new RTCPeerConnection({iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]});
+        // const pc = new RTCPeerConnection({iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]});
+        const pc = new RTCPeerConnection({
+          iceServers: [
+            { urls: "stun:stun.l.google.com:19302" },
+            {
+              urls: "turn:openrelay.metered.ca:80",
+              username: "openrelayproject",
+              credential: "openrelayproject"
+            }
+          ]
+        });
         peerConnectionRef.current = pc;
 
         stream?.getTracks().forEach((track) => pc.addTrack(track, stream));
